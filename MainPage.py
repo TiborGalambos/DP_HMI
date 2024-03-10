8e00823aafc287abc47d9ab755e76a0d6c11c05a
TiborGalambos <tibor122333@gmail.com>
2024-03-10 21:24:51 +0100

    deleted some comments

--- MainPage.py
+++ MainPage.py
@@ -28,20 +28,6 @@ class MainPage(ctk.CTkFrame, PageController):
         # self.grid_rowconfigure(2, weight=1)
         self.grid_columnconfigure(2, weight=1)
 
-
-        # self.grid_rowconfigure(1, weight=2)
-
-        # buttons_info = {
-        #     # "MainPage": "MainPage",
-        #     "Page1": "SubPage1",
-        #     "Page2": "SubPage2",
-        #     "Page3": "SubPage3",
-        #     "Page4": "SubPage4",
-        #     "Page5": "SubPage5",
-        # }
-
-        # button_size = 100
-
         col_num = 0
         row_num = 0
         for text, page_name in self.buttons_info.items():
@@ -61,29 +47,4 @@ class MainPage(ctk.CTkFrame, PageController):
 
 
 
-
-
-        # buttons_info = {
-        #     "SubPage 1": "SubPage1",
-        #     "SubPage 2": "SubPage2",
-        #     "SubPage 3": "SubPage3",
-        #     "SubPage 4": "SubPage4",
-        #     "SubPage 5": "SubPage5"
-        # # }
-        #
-        #
-        # button1 = ctk.CTkButton(self, text="Button1", font=('Arial', 16))
-        # button1.grid(column=0, row=0, sticky='nsew', padx = 50, pady = 50)
-        #
-        # button2 = ctk.CTkButton(self, text="Button2", font=('Arial', 16))
-        # button2.grid(column=1, row=0, sticky='nsew', padx = 50, pady = 50)
-        #
-        # button3 = ctk.CTkButton(self, text="Button3", font=('Arial', 16))
-        # button3.grid(column=2, row=0, sticky='nsew', padx = 50, pady = 50)
-        #
-        # button4 = ctk.CTkButton(self, text="Button4", font=('Arial', 16))
-        # button4.grid(column=0, row=1, sticky='nsew', padx = 50, pady = 50)
-        #
-        # button5 = ctk.CTkButton(self, text="Button5", font=('Arial', 16))
-        # button5.grid(column=1, row=1, sticky='nsew', padx = 50, pady = 50)
 
